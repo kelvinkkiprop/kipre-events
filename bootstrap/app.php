a96e1e8981fbb09fd2d038be2aebbcbd0e3bc82a
kelvinkkiprop <kelvinkiprop5@gmail.com>
2025-10-07 10:23:47 +0300

    DD

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,7 +11,11 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        //
+        // Add
+        $middleware->alias([
+            'adminonly'=>\App\Http\Middleware\AdminsOnly::class,
+            // 'notvisitors'=>\App\Http\Middleware\NotVisitorsOnly::class,
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
