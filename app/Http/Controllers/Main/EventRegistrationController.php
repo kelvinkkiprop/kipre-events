81de24813dd4dbb3d7bbf55bc52e16eec488c06c
kelvinkkiprop <kelvinkiprop5@gmail.com>
2025-10-07 10:24:27 +0300

    DD

--- app/Http/Controllers/Main/EventRegistrationController.php
+++ app/Http/Controllers/Main/EventRegistrationController.php
@@ -24,6 +24,7 @@ class EventRegistrationController extends Controller
     public function __construct()
     {
         $this->middleware('auth');
+        $this->middleware('adminonly');
     }
 
     /**
